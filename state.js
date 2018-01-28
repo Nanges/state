ee05e314028d419f3c7f4a1ca120a611697eea9e
Nanges <laha.olivier@gmail.com>
2018-01-28 01:20:55 +0100

    Update state manager + setup exemple usages

--- state.js
+++ state.js
@@ -16,6 +16,7 @@ class Command
 class StateManager
 {
     constructor(){
+        this._currentState = null;
         this._states = {};
     }
 
@@ -23,4 +24,59 @@ class StateManager
         if(this._states.hasOwnProperty(name)) return;
         this._states[name] = commands;
     }
+
+    setState(newState){
+        if(this._currentState === newState || !this._states.hasOwnProperty(newState)) return;
+
+        let osCommands = this._states[this._currentState] || [];
+        let nsCommands = this._states[newState];
+
+        this.dispatch(osCommands, nsCommands);
+        this._currentState = newState;
+    }
+
+    dispatch(osCommands, nsCommands){
+        
+        //undo old state commands
+        osCommands
+        .diff(nsCommands)
+        .forEach(c => c.undo());
+
+        //redo new state commands
+        nsCommands
+        .diff(osCommands)
+        .forEach(c => c.redo());
+
+    }
 }
+
+var c1 = new Command(
+    () => console.log('Open light'),
+    () => console.log('Close light')
+);
+
+var c2 = new Command(
+    () => console.log('Open door'),
+    () => console.log('Close door')
+);
+
+var c3 = new Command(
+    () => console.log('Open window'),
+    () => console.log('Close window')
+);
+
+var sm = new StateManager();
+
+sm.register("STATE 1", [c1,c2]);
+sm.register("STATE 2", [c3,c2]);
+sm.register("STATE 3", [c1,c3, c2]);
+sm.register("STATE 4", []);
+
+console.log("=== STATE 1 ===");
+sm.setState("STATE 1");
+console.log("=== STATE 3 ===");
+sm.setState("STATE 3");
+console.log("=== STATE 2 ===");
+sm.setState("STATE 2");
+console.log("=== STATE 4 ===");
+sm.setState("STATE 4");
